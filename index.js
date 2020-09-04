6f3835a382fd95fece065f78b59d205fdffd7f28
Shad Musthafa <shad.musthafa@stellium.com>
2020-09-04 18:02:00 +0530

    Messaging queue set up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 app.use(bodyParser.json({
-    linit: '1mb'
+    limit: '1mb'
 }));
 app.use(bodyParser.urlencoded({
     extended: false
