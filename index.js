1d6b1dff59f9fe27ba5363a00771bddc02287a9e
Shad Musthafa <shad.musthafa@stellium.com>
2020-09-04 17:06:26 +0530

    Smart app server copied

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const app =express();
+const app = express();
+app.use(bodyParser.json({
+    linit: '1mb'
+}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static(__dirname + '/public'));
 
-app.get("/",(req,res)=>{
+let connections = [];
+app.get("/connect", (req, res) => {
+    var token = "T-" + String(Math.random()).substring(2);
+    connections[token] = [];
+    res.json({
+        "token": token
+    });
+});
+
+app.get("/action", (req, res) => {
+    var token = req.query.token;
+    var keep = req.query.keep;
+    var actions = connections[token];
+    if (!keep) {
+        connections[token] = [];
+    }
+
+    res.json({
+        "actions": actions
+    });
+});
+
+app.get("/", (req, res) => {
     res.send("Hello World");
-})
+});
+
+app.post("/action",(req,res)=>{
+    var token = req.body.token;
+    var actions = connections[token];
+    if(actions){
+        connections[token] = actions.concat(req.body.actions);
+        actions = connections[token];
+        res.json({
+            actions: actions
+        });
+    }else{
+        res.json({
+            error:"Invalid token"
+        });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log(`App initialized and listening on port ${PORT}`)
 })
